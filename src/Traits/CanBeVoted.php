82f71bf7ad7fc848b4281b961ca1e2c1ae545f9e
安正超 <anzhengchao@gmail.com>
2018-03-05 13:57:11 +0800

    Apply fixes from StyleCI (#28)

--- src/Traits/CanBeVoted.php
+++ src/Traits/CanBeVoted.php
@@ -29,30 +29,30 @@ trait CanBeVoted
     {
         return Follow::isRelationExists($this, 'voters', $user);
     }
-	
-	/**
-	 * Check if item is upvoted by given user.
-	 *
-	 * @param int $user
-	 *
-	 * @return bool
-	 */
-	public function isUpvotedBy($user)
-	{
-		return Follow::isRelationExists($this, 'upvoters', $user);
-	}
-	
-	/**
-	 * Check if item is downvoted by given user.
-	 *
-	 * @param int $user
-	 *
-	 * @return bool
-	 */
-	public function isDownvotedBy($user)
-	{
-		return Follow::isRelationExists($this, 'downvoters', $user);
-	}
+
+    /**
+     * Check if item is upvoted by given user.
+     *
+     * @param int $user
+     *
+     * @return bool
+     */
+    public function isUpvotedBy($user)
+    {
+        return Follow::isRelationExists($this, 'upvoters', $user);
+    }
+
+    /**
+     * Check if item is downvoted by given user.
+     *
+     * @param int $user
+     *
+     * @return bool
+     */
+    public function isDownvotedBy($user)
+    {
+        return Follow::isRelationExists($this, 'downvoters', $user);
+    }
 
     /**
      * Return voters.
@@ -65,28 +65,28 @@ trait CanBeVoted
                     ->wherePivotIn('relation', [Follow::RELATION_UPVOTE, Follow::RELATION_DOWNVOTE])
                     ->withPivot('followable_type', 'relation', 'created_at');
     }
-	
-	/**
-	 * Return upvoters.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function upvoters()
-	{
-		return $this->morphToMany(config('follow.user_model'), config('follow.morph_prefix'), config('follow.followable_table'))
-		            ->wherePivot('relation', '=', Follow::RELATION_UPVOTE)
-		            ->withPivot('followable_type', 'relation', 'created_at');
-	}
-	
-	/**
-	 * Return downvoters.
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function downvoters()
-	{
-		return $this->morphToMany(config('follow.user_model'), config('follow.morph_prefix'), config('follow.followable_table'))
-		            ->wherePivot('relation', '=', Follow::RELATION_DOWNVOTE)
-		            ->withPivot('followable_type', 'relation', 'created_at');
-	}
+
+    /**
+     * Return upvoters.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function upvoters()
+    {
+        return $this->morphToMany(config('follow.user_model'), config('follow.morph_prefix'), config('follow.followable_table'))
+                    ->wherePivot('relation', '=', Follow::RELATION_UPVOTE)
+                    ->withPivot('followable_type', 'relation', 'created_at');
+    }
+
+    /**
+     * Return downvoters.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function downvoters()
+    {
+        return $this->morphToMany(config('follow.user_model'), config('follow.morph_prefix'), config('follow.followable_table'))
+                    ->wherePivot('relation', '=', Follow::RELATION_DOWNVOTE)
+                    ->withPivot('followable_type', 'relation', 'created_at');
+    }
 }
--- src/Traits/CanVote.php
+++ src/Traits/CanVote.php
@@ -29,38 +29,38 @@ trait CanVote
      */
     public function vote($targets, $type = 'upvote', $class = __CLASS__)
     {
-	    $this->cancelVote($targets);
-    	
+        $this->cancelVote($targets);
+
         return Follow::attachRelations($this, str_plural($type), $targets, $class);
     }
-	
-	/**
-	 * Upvote an item or items.
-	 *
-	 * @param int|array|\Illuminate\Database\Eloquent\Model $targets
-	 * @param string                                        $type
-	 * @param string                                        $class
-	 *
-	 * @return array
-	 */
-	public function upvote($targets, $class = __CLASS__)
-	{
-		return $this->vote($targets, 'upvote', $class);
-	}
-	
-	/**
-	 * Downvote an item or items.
-	 *
-	 * @param int|array|\Illuminate\Database\Eloquent\Model $targets
-	 * @param string                                        $type
-	 * @param string                                        $class
-	 *
-	 * @return array
-	 */
-	public function downvote($targets, $class = __CLASS__)
-	{
-		return $this->vote($targets, 'downvote', $class);
-	}
+
+    /**
+     * Upvote an item or items.
+     *
+     * @param int|array|\Illuminate\Database\Eloquent\Model $targets
+     * @param string                                        $type
+     * @param string                                        $class
+     *
+     * @return array
+     */
+    public function upvote($targets, $class = __CLASS__)
+    {
+        return $this->vote($targets, 'upvote', $class);
+    }
+
+    /**
+     * Downvote an item or items.
+     *
+     * @param int|array|\Illuminate\Database\Eloquent\Model $targets
+     * @param string                                        $type
+     * @param string                                        $class
+     *
+     * @return array
+     */
+    public function downvote($targets, $class = __CLASS__)
+    {
+        return $this->vote($targets, 'downvote', $class);
+    }
 
     /**
      * Cancel vote for an item or items.
@@ -74,8 +74,8 @@ trait CanVote
     {
         Follow::detachRelations($this, 'upvotes', $targets, $class);
         Follow::detachRelations($this, 'downvotes', $targets, $class);
-	
-	    return $this;
+
+        return $this;
     }
 
     /**
@@ -90,33 +90,33 @@ trait CanVote
     {
         return Follow::isRelationExists($this, 'upvote', $target, $class);
     }
-	
-	/**
-	 * Check if user is downvoted given item.
-	 *
-	 * @param int|array|\Illuminate\Database\Eloquent\Model $target
-	 * @param string                                        $class
-	 *
-	 * @return bool
-	 */
-	public function hasDownvoted($target, $class = __CLASS__)
-	{
-		return Follow::isRelationExists($this, 'downvote', $target, $class);
-	}
-	
-	/**
-	 * Return item votes.
-	 *
-	 * @param string $class
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function votes($class = __CLASS__)
-	{
-		return $this->morphedByMany($class, config('follow.morph_prefix'), config('follow.followable_table'))
-					->wherePivotIn('relation', [Follow::RELATION_UPVOTE, Follow::RELATION_DOWNVOTE])
-		            ->withPivot('followable_type', 'relation', 'created_at');
-	}
+
+    /**
+     * Check if user is downvoted given item.
+     *
+     * @param int|array|\Illuminate\Database\Eloquent\Model $target
+     * @param string                                        $class
+     *
+     * @return bool
+     */
+    public function hasDownvoted($target, $class = __CLASS__)
+    {
+        return Follow::isRelationExists($this, 'downvote', $target, $class);
+    }
+
+    /**
+     * Return item votes.
+     *
+     * @param string $class
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function votes($class = __CLASS__)
+    {
+        return $this->morphedByMany($class, config('follow.morph_prefix'), config('follow.followable_table'))
+                    ->wherePivotIn('relation', [Follow::RELATION_UPVOTE, Follow::RELATION_DOWNVOTE])
+                    ->withPivot('followable_type', 'relation', 'created_at');
+    }
 
     /**
      * Return item upvotes.
@@ -131,18 +131,18 @@ trait CanVote
                     ->wherePivot('relation', '=', Follow::RELATION_UPVOTE)
                     ->withPivot('followable_type', 'relation', 'created_at');
     }
-	
-	/**
-	 * Return item downvotes.
-	 *
-	 * @param string $class
-	 *
-	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
-	 */
-	public function downvotes($class = __CLASS__)
-	{
-		return $this->morphedByMany($class, config('follow.morph_prefix'), config('follow.followable_table'))
-		            ->wherePivot('relation', '=', Follow::RELATION_DOWNVOTE)
-		            ->withPivot('followable_type', 'relation', 'created_at');
-	}
+
+    /**
+     * Return item downvotes.
+     *
+     * @param string $class
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function downvotes($class = __CLASS__)
+    {
+        return $this->morphedByMany($class, config('follow.morph_prefix'), config('follow.followable_table'))
+                    ->wherePivot('relation', '=', Follow::RELATION_DOWNVOTE)
+                    ->withPivot('followable_type', 'relation', 'created_at');
+    }
 }
