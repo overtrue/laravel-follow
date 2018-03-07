998f4f24cb68493159e4c9eba63c1974024b2215
overtrue <anzhengchao@gmail.com>
2018-03-07 23:53:09 +0800

    Fixed #38

--- src/Traits/CanSubscribe.php
+++ src/Traits/CanSubscribe.php
@@ -25,6 +25,7 @@ trait CanSubscribe
      * @param string                                        $class
      *
      * @return array
+     * @throws \Exception
      */
     public function subscribe($targets, $class = __CLASS__)
     {
@@ -51,6 +52,7 @@ trait CanSubscribe
      * @param string                                        $class
      *
      * @return array
+     * @throws \Exception
      */
     public function toggleSubscribe($targets, $class = __CLASS__)
     {
--- src/Traits/CanVote.php
+++ src/Traits/CanVote.php
@@ -26,6 +26,7 @@ trait CanVote
      * @param string                                        $class
      *
      * @return array
+     * @throws \Exception
      */
     public function vote($targets, $type = 'upvote', $class = __CLASS__)
     {
@@ -41,6 +42,7 @@ trait CanVote
      * @param string                                        $class
      *
      * @return array
+     * @throws \Exception
      */
     public function upvote($targets, $class = __CLASS__)
     {
@@ -54,6 +56,7 @@ trait CanVote
      * @param string                                        $class
      *
      * @return array
+     * @throws \Exception
      */
     public function downvote($targets, $class = __CLASS__)
     {
