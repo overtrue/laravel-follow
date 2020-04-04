3bd8e578bbfca4a9b4ce3427dffb9a6eb0e91970
overtrue <anzhengchao@gmail.com>
2020-04-05 01:42:45 +0800

    CS.

--- src/Events/Followed.php
+++ src/Events/Followed.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * This file is part of the overtrue/laravel-follow
+ *  This file is part of the overtrue/laravel-follow
  *
- * (c) overtrue <i@overtrue.me>
+ *  (c) overtrue <i@overtrue.me>
  *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
+ *  This source file is subject to the MIT license that is bundled
+ *  with this source code in the file LICENSE.
  */
 
 namespace Overtrue\LaravelFollow\Events;
--- src/FollowServiceProvider.php
+++ src/FollowServiceProvider.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * This file is part of the overtrue/laravel-follow
+ *  This file is part of the overtrue/laravel-follow
  *
- * (c) overtrue <i@overtrue.me>
+ *  (c) overtrue <i@overtrue.me>
  *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
+ *  This source file is subject to the MIT license that is bundled
+ *  with this source code in the file LICENSE.
  */
 
 namespace Overtrue\LaravelFollow;
@@ -24,11 +24,11 @@ class FollowServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            \dirname(__DIR__).'/migrations/' => database_path('migrations'),
+            \dirname(__DIR__) . '/migrations/' => database_path('migrations'),
         ], 'migrations');
 
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(\dirname(__DIR__).'/migrations/');
+            $this->loadMigrationsFrom(\dirname(__DIR__) . '/migrations/');
         }
     }
 }
