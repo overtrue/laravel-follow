7c8b0a49d5c199c92532f5859b1b54cafe7326fa
overtrue <anzhengchao@gmail.com>
2020-04-04 17:20:46 +0000

    Apply fixes from StyleCI

--- src/FollowServiceProvider.php
+++ src/FollowServiceProvider.php
@@ -1,11 +1,12 @@
 <?php
 
 /*
- * This file is part of the overtrue/laravel-followable.
+ * This file is part of the overtrue/laravel-follow
  *
- * (c) overtrue <anzhengchao@gmail.com>
+ * (c) overtrue <i@overtrue.me>
  *
- * This source file is subject to the MIT license that is bundled.
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
  */
 
 namespace Overtrue\LaravelFollow;
@@ -23,11 +24,11 @@ class FollowServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            \dirname(__DIR__) . '/migrations/' => database_path('migrations'),
+            \dirname(__DIR__).'/migrations/' => database_path('migrations'),
         ], 'migrations');
 
         if ($this->app->runningInConsole()) {
-            $this->loadMigrationsFrom(\dirname(__DIR__) . '/migrations/');
+            $this->loadMigrationsFrom(\dirname(__DIR__).'/migrations/');
         }
     }
 }
